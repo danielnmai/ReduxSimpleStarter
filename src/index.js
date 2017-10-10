1a175b3d9d05adbb77c8eb956d56a2d30957bd38
Daniel Mai <danielmai1728@gmail.com>
2017-10-09 17:44:37 -0700

    update components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,43 @@
-import React from 'react';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware } from 'redux';
+//
+// import App from './components/app';
+// import reducers from './reducers';
+//
+// const createStoreWithMiddleware = applyMiddleware()(createStore);
+//
+// ReactDOM.render(
+//   <Provider store={createStoreWithMiddleware(reducers)}>
+//     <App />
+//   </Provider>
+//   , document.querySelector('.container'));
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyDxA9yCsd_QbTC_ZP02KNm5mtdq8B0QRiE';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { videos: [] };
+    YTSearch({key: API_KEY, term: 'surfboards'},  (videos) => {
+      this.setState({ videos });
+      console.log(videos);
+    })
+  }
+  render(){
+    return (
+    <div>
+      <SearchBar />
+      <VideoList videos = {this.state.videos}/>
+    </div>
+  );
+  }
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+}
+ReactDOM.render(<App />, document.getElementById('root'));
