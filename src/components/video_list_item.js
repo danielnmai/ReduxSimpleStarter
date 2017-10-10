f437cbcb6bdabb99e9da294e380d78917a672ec4
Daniel Mai <danielmai1728@gmail.com>
2017-10-09 20:42:26 -0700

    add video selection function

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
-const VideoListItem = ({video}) => {
-  //const video = props.video
-  console.log({video});
-  return <li className="list-group-item">
+const VideoListItem = ({video, onVideoSelect}) => {
+  //const video = props.video is equal to ({video}) in function argument. ES6 syntax only.
+  const imgURL = video.snippet.thumbnails.default.url
+  return <li onClick={()=> onVideoSelect(video)}className="list-group-item">
     <div className="video-list media">
       <div className="media-left">
-        <img className="media-object"/>
+        <img className="media-object" src={imgURL}/>
       </div>
       <div className="media-body">
-        <div className = "media-heading"></div>
+        <div className = "media-heading">{video.snippet.title}</div>
       </div>
-
-
     </div>
    </li>
 };
--- src/index.js
+++ src/index.js
@@ -3,26 +3,38 @@ import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDxA9yCsd_QbTC_ZP02KNm5mtdq8B0QRiE';
 
 class App extends Component {
   constructor(props){
     super(props);
-    this.state = { videos: [] };
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+     };
+
     YTSearch({key: API_KEY, term: 'surfboards'},  (videos) => {
-      this.setState({ videos });
-      console.log(videos);
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+
+       });
     })
   }
   render(){
     return (
-    <div>
-      <SearchBar />
-      <VideoList videos = {this.state.videos}/>
-    </div>
-  );
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+          videos = {this.state.videos}/>
+      </div>
+    );
   }
 
 }
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.querySelector('.container'));
