3e27d1868dc5c633addd7d27445069937f5b6fbd
Daniel Mai <danielmai1728@gmail.com>
2017-10-09 19:34:04 -0700

    fix project startup

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -8,7 +8,6 @@ const VideoListItem = ({video}) => {
       <div className="media-left">
         <img className="media-object"/>
       </div>
-
       <div className="media-body">
         <div className = "media-heading"></div>
       </div>
